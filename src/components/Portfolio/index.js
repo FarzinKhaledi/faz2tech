fe5616c7d71814a0f23cb2b63fef365418a3c161
Farzin khaledi <Khaledifarzin1@gmail.com>
2023-02-13 12:38:40 +0000

    done

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from 'react';
+import Loader from 'react-loaders';
+import AnimatedLetters from '../AnimatedLetters';
+import './index.scss';
+import { onSnapshot, collection } from 'firebase/firestore';
+import db from '../../firebase';
+
+const Portfolio = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+  const [portfolio, setPortfolio] = useState([]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+
+  // useEffect(() => {
+  //     getPortfolio();
+  // }, []);
+
+  // const getPortfolio = async () => {
+  //     const querySnapshot = await getDocs(collection(db, ''));
+  //     setPortfolio(querySnapshot.docs.map((doc) => doc.data()));
+  // }
+
+  useEffect(() => {
+    const getPortfolio = onSnapshot(
+      collection(db, 'projects'),
+      (snapShot) => {
+        setPortfolio(snapShot.docs.map((doc) => doc.data()));
+      },
+      (error) => {
+        throw error;
+      }
+    );
+    return () => {
+      getPortfolio();
+    };
+  }, [portfolio]);
+
+  const renderPortfolio = (portfolio) => {
+    return (
+      <div className="images-container">
+        {portfolio.map((port, idx) => {
+          return (
+            <div className="image-box" key={idx}>
+              <img
+                src={port.image}
+                className="portfolio-image"
+                alt="portfolio"
+              />
+              <div className="content">
+                <p className="title">{port.name}</p>
+                <h4 className="description">{port.description}</h4>
+                <button className="btn" onClick={() => window.open(port.url)}>
+                  View Code
+                </button>
+                <button
+                  className="btn"
+                  onClick={() => window.open(port.deploy)}
+                >
+                  View
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <div className="container portfolio-page">
+        <h1 className="page-title">
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={'Portfolio'.split('')}
+            idx={15}
+          />
+        </h1>
+        <div>{renderPortfolio(portfolio)}</div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  );
+};
+
+export default Portfolio;
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from '@firebase/firestore';
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  databaseURL: `${process.env.REACT_APP_DATABASE_URL}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export default db;
